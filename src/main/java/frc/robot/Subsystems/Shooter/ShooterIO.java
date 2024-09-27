656ef859dc36412a054c6109c772d2a464fc5b62
Andrew-6657 <andrew.frankovic@uphsconnect.org>
2024-09-27 08:33:44 -0700

    Forgot to commit

--- src/main/java/frc/robot/Subsystems/Shooter/ShooterIO.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterIO.java
@@ -4,4 +4,16 @@ import org.littletonrobotics.junction.AutoLog;
 
 public interface ShooterIO {
     
+
+    @AutoLog
+  public static class ShooterIOInputs {
+    public double flywheelMotorVelocity = 0.0; // RPM
+    public double flywheelMotorTemp = 0.0; // Celcius
+    public double flywheelMotorVoltage = 0.0; // Volts
+    public double flywheelMotorCurrent = 0.0; // Amps
+    public boolean flywheelAtSetpoint = false;
+  }
+
+
+
 }
