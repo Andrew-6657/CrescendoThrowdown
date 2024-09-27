2e1e04d3994e1763f83156c1a363949aa59f3f18
Andrew-6657 <andrew.frankovic@uphsconnect.org>
2024-09-27 08:41:43 -0700

    Finished ShooterIO.java

--- src/main/java/frc/robot/Subsystems/Shooter/ShooterIO.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterIO.java
@@ -11,9 +11,12 @@ public interface ShooterIO {
     public double flywheelMotorTemp = 0.0; // Celcius
     public double flywheelMotorVoltage = 0.0; // Volts
     public double flywheelMotorCurrent = 0.0; // Amps
-    public boolean flywheelAtSetpoint = false;
+    public boolean flywheelAtSetpoint = false; // I don't think this is necessary for a flywheel, I will probably delete it later
   }
 
+  public default void updateInputs(ShooterIOInputs inputs) {}
+
+  public default void changeVelocityTarget(double velocityTarget) {}//Might want to change the variable name from "velocityTarget" to something else like "rpmTarget"
 
 
 }
