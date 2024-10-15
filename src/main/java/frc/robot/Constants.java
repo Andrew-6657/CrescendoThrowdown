12de76dd6ce150d7d3d7d67341b8a18b4d6abb8d
Andrew-6657 <andrew.frankovic@uphsconnect.org>
2024-10-14 17:23:52 -0700

    Huge addition to Shooter Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+
 /** Add your docs here. */
 public class Constants {
 
@@ -58,11 +61,72 @@ public class Constants {
         }
     }
 
-    public static class IntakeConstants {
+    public static class PivoteConstants {
     
         //pivote contants
 
         //kick constants (flywheal?)
     
       }
+    public static class ShooterConstants{
+      public static class RightFlywheel{
+        public static final double kGearing = (1d / 2); //note, ask if this should be changed to a 2d / 1
+        
+        public static final double kMinRpm = -3190;
+        public static final double kMaxRpm = 3190; //these need to be configured
+
+        public static final double kCurrentLimit = 30;
+
+        public static final double kFeedRPM = 300; //these need to be configured
+        public static final double kSpeakerRPM = 2950;
+        public static final double kAmpRPM = 1000;
+
+        public static final CurrentLimitsConfigs kCurrentConfigs =
+        new CurrentLimitsConfigs()
+            .withStatorCurrentLimit(kCurrentLimit * 2)
+            .withSupplyCurrentLimit(kCurrentLimit)
+            .withStatorCurrentLimitEnable(true)
+            .withSupplyCurrentLimitEnable(true)
+            .withSupplyCurrentThreshold(kCurrentLimit)
+            .withSupplyTimeThreshold(0);
+
+        public static Slot0Configs kSlot0 =
+        new Slot0Configs() //these values need to be tuned
+            .withKS(.05)
+            .withKV(12d / ((6380d / 60) * kGearing)) // Volts/Mechanism RPS
+            .withKP(0)
+            .withKI(0)
+            .withKD(0);
+      }
+      public static class LeftFlywheel{
+        public static final double kGearing = (1d / 1); //I think that this one is a 1 to 1 ratio
+        
+        public static final double kMinRpm = -3190;
+        public static final double kMaxRpm = 3190; //these need to be configured
+
+        public static final double kCurrentLimit = 30;
+
+        public static final double kFeedRPM = 300; //these need to be configured
+        public static final double kSpeakerRPM = 2950;
+        public static final double kAmpRPM = 1000;
+
+        public static final CurrentLimitsConfigs kCurrentConfigs =
+        new CurrentLimitsConfigs()
+            .withStatorCurrentLimit(kCurrentLimit * 2)
+            .withSupplyCurrentLimit(kCurrentLimit)
+            .withStatorCurrentLimitEnable(true)
+            .withSupplyCurrentLimitEnable(true)
+            .withSupplyCurrentThreshold(kCurrentLimit)
+            .withSupplyTimeThreshold(0);
+
+        public static Slot0Configs kSlot0 =
+        new Slot0Configs() //these values need to be tuned
+            .withKS(.05)
+            .withKV(12d / ((6380d / 60) * kGearing)) // Volts/Mechanism RPS
+            .withKP(0)
+            .withKI(0)
+            .withKD(0);
+      }
+    }
+    
 }
--- src/main/java/frc/robot/Subsystems/Shooter/ShooterIO_Real.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterIO_Real.java
@@ -4,9 +4,15 @@ package frc.robot.Subsystems.Shooter;
 
 import org.littletonrobotics.junction.AutoLogOutput;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.Constants.RobotConstants.CANID;
 
 public class ShooterIO_Real implements ShooterIO {
@@ -30,13 +36,36 @@ private double rpmSetpoint = 0;
 private VelocityVoltage rightWheelSetpoint = new VelocityVoltage(0).withSlot(0);
 private VelocityVoltage leftWheelSetpoint = new VelocityVoltage(0).withSlot(0);
 
+// Chamber Beam Break Sensor
+  DigitalInput beambreak = new DigitalInput(8);
 
 
 
 
 
 
+public ShooterIO_Real() {
 
+    // Motor Controller Configurations
+
+    
+
+    
+
+    
+
+    // Configure the leading flywheel motor
+    var rightFlywheelConfigurator = rightFlywheel.getConfigurator();
+    var rightFlywheelConfig = new TalonFXConfiguration();
+    rightFlywheelConfig.Feedback.SensorToMechanismRatio =
+        1.0 / ShooterConstants.RightFlywheel.kGearing; // Sets default output to flywheel rotations
+    rightFlywheelConfig.Slot0 = ShooterConstants.RightFlywheel.kSlot0; // PID Constants
+    rightFlywheelConfig.CurrentLimits = ShooterConstants.RightFlywheel.kCurrentConfigs; // Current Limits
+    rightFlywheelConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    rightFlywheelConfigurator.apply(rightFlywheelConfig);
+
+    
+  }
 
 
 
